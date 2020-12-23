992f9e413a9724d349fbcc14b08b3500ba686633
Faith Kane <faith.x.kane@gmail.com>
2020-12-23 10:48:01 -0600

    Commit wednesday query using self-join

--- hacker_rank.sql
+++ hacker_rank.sql
@@ -96,3 +96,25 @@ FROM students
 JOIN grades
 WHERE marks BETWEEN min_mark AND max_mark
 ORDER BY grade DESC, name;
+
+/*
+Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand.
+
+Hermione decides the best way to choose is by determining the minimum number of gold galleons needed to buy each non-evil wand of high power and age. Write a query to print the id, age, coins_needed, and power of the wands that Ron's interested in, sorted in order of descending power. If more than one wand has same power, sort the result in order of descending age.
+
+This query requires a self-join to obtain the minimum number of coins for power and age.
+*/
+
+SELECT w.id,
+       wp.age,
+       w.coins_needed,
+       w.power
+FROM wands AS w
+JOIN wands_property AS wp ON w.code=wp.code 
+WHERE wp.is_evil = 0
+    AND w.coins_needed = (SELECT MIN(coins_needed)
+                          FROM wands AS w2
+                          JOIN wands_property AS wp2 ON w2.code=wp2.code
+                          WHERE w.power=w2.power
+                          AND wp.age=wp2.age)
+ORDER BY w.power DESC, wp.age DESC;
