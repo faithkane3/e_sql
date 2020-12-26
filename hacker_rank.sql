7c48b866dff9edcaa6c25f4f215780e4ccf3267c
Faith Kane <faith.x.kane@gmail.com>
2020-12-25 19:56:59 -0600

    Commit problem I couldnt solve

--- hacker_rank.sql
+++ hacker_rank.sql
@@ -141,3 +141,12 @@ INNER JOIN
 GROUP BY h.hacker_id, name
 HAVING total_score > 0
 ORDER BY total_score DESC, h.hacker_id ASC;
+
+/*
+Julia asked her students to create some coding challenges. Write a query to print the hacker_id, name, and the total number of challenges created by each student. 
+
+Sort your results by the total number of challenges in descending order. If more than one student created the same number of challenges, then sort the result by hacker_id. 
+
+If more than one student created the same number of challenges and the count is less than the maximum number of challenges created, then exclude those students from the result.
+*/
+
