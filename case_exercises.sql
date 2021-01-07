bd565dabadda2cbbb49a88b17ba7a41c284cdc3b
Faith Kane <faith.x.kane@gmail.com>
2021-01-07 15:31:49 -0600

    Commit second draft adding if statement to 2

--- case_exercises.sql
+++ case_exercises.sql
@@ -2,7 +2,7 @@
 -- (Use CASE statements or IF() function to explore information in the employees database
 )
 
--- 1. Write a query that returns all employees (emp_no), their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not.
+-- 1. Write a query that returns all employees (emp_no), their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not.   (331_603 records returned)
 
 USE employees;
 
@@ -24,7 +24,7 @@ SELECT
 	CASE
 		WHEN to_date = '9999-01-01' THEN 1
 		ELSE 0
-	END AS 'is_current_employee'
+	END AS is_current_employee
 FROM dept_emp
 JOIN (SELECT 
 		emp_no,
@@ -32,6 +32,23 @@ JOIN (SELECT
 	FROM dept_emp
 	GROUP BY emp_no) AS last_dept
 ON dept_emp.emp_no = last_dept.emp_no;
+
+-- Using IF() function.
+
+SELECT
+	dept_emp.emp_no,
+	dept_no,
+	to_date,
+	from_date,
+	IF(to_date = '9999-01-01', 1, 0) AS is_current_employee
+FROM dept_emp
+JOIN (SELECT 
+		emp_no,
+		MAX(to_date) AS max_date
+	FROM dept_emp
+	GROUP BY emp_no) AS last_dept
+ON dept_emp.emp_no = last_dept.emp_no;
+
 
 -- 2. Write a query that returns all employee names (previous and current), and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name.
 
