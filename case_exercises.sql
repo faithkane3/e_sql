2c0231a3d3a176806a799079361a4a3655891ef7
Faith Kane <faith.x.kane@gmail.com>
2021-01-08 05:45:45 -0600

    Commit add hire date

--- case_exercises.sql
+++ case_exercises.sql
@@ -2,7 +2,7 @@
 -- (Use CASE statements or IF() function to explore information in the employees database
 )
 
--- 1. Write a query that returns all employees (emp_no), their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not.   (331_603 records returned)
+-- 1. Write a query that returns all employees (emp_no), their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not.   (300_024 records returned without duplicate departments for any employee)
 
 USE employees;
 
@@ -14,26 +14,7 @@ SELECT
 FROM dept_emp
 GROUP BY emp_no;
 
--- Use my subquery to get rid of the duplicate entries for employees older departments.
-
-SELECT
-	dept_emp.emp_no,
-	dept_no,
-	to_date,
-	from_date,
-	CASE
-		WHEN to_date = '9999-01-01' THEN 1
-		ELSE 0
-	END AS is_current_employee
-FROM dept_emp
-JOIN (SELECT 
-		emp_no,
-		MAX(to_date) AS max_date
-	FROM dept_emp
-	GROUP BY emp_no) AS last_dept
-ON dept_emp.emp_no = last_dept.emp_no;
-
--- Using IF() function.
+-- Use my subquery to get rid of the duplicate entries for employees older departments. BUT this is not the employees hire_date.
 
 SELECT
 	dept_emp.emp_no,
@@ -47,7 +28,27 @@ JOIN (SELECT
 		MAX(to_date) AS max_date
 	FROM dept_emp
 	GROUP BY emp_no) AS last_dept
-ON dept_emp.emp_no = last_dept.emp_no;
+ON dept_emp.emp_no = last_dept.emp_no
+	AND dept_emp.to_date = last_dept.max_date;
+
+SELECT
+	dept_emp.emp_no,
+	dept_no,
+	to_date,
+	from_date,
+	hire_date,
+	IF(to_date = '9999-01-01', 1, 0) AS is_current_employee,
+	IF(hire_date = from_date, 1, 0) AS only_one_dept
+FROM dept_emp
+JOIN (SELECT 
+		emp_no,
+		MAX(to_date) AS max_date
+	FROM dept_emp
+	GROUP BY emp_no) AS last_dept
+ON dept_emp.emp_no = last_dept.emp_no
+	AND dept_emp.to_date = last_dept.max_date
+JOIN employees AS e ON e.emp_no = dept_emp.emp_no;
+
 
 
 -- 2. Write a query that returns all employee names (previous and current), and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name.
